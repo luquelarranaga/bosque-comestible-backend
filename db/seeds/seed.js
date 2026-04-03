c3f9906dbbd28b06192ac93c68dc754277abd9a5
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-03 19:05:47 +0100

    created seeds.js files and created query to create and seed table

---
+++ db/seeds/seed.js
@@ -0,0 +1,32 @@
+const db = require("../connection");
+const format = require("pg-format");
+
+const seed = async ({ plants }) => {
+  await db.query("DROP TABLE IF EXISTS plants");
+
+  await db.query(`
+    CREATE TABLE plants (
+    plant_id SERIAL PRIMARY KEY,
+    species VARCHAR NOT NULL,
+    date_planted TIMESTAMP,
+    coordinates VARCHAR,
+    body VARCHAR,
+    care VARCHAR,
+    img_url VARCHAR
+)`);
+};
+
+const insertPlantsQuery = format(
+  `INSERT INTO plants (species, date_planted, coordinates, body, care, img_url) VALUE %L RETURNING *;`,
+  plants.map(({ species, date_planted, coordinates, body, care, img_url }) => [
+    species,
+    date_planted,
+    coordinates,
+    body,
+    care,
+    img_url,
+  ]),
+);
+await db.query(insertPlantsQuery);
+
+module.exports = seed;
