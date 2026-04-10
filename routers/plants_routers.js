6e3a23f22da8b59d11d01b0596045510beb7ee5c
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-10 16:50:43 +0100

    updated /api/plants/ router to handle invalid methods

--- routers/plants_routers.js
+++ routers/plants_routers.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const getAllPlants = require("../controllers/plants_controllers");
+const handleInvalidMethods = require("../utils/handleInvalidMethods");
 
 //add handle invalid methods.
-router.route("/").get(getAllPlants);
+router.route("/").get(getAllPlants).all(handleInvalidMethods);
 
 module.exports = router;
