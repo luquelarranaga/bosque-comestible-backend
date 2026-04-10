dcf277e7399b5eceaf176c2eb6ba77020aa9a3f5
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-10 14:40:39 +0100

    created test to get all plants

--- __tests__/app_tests/app.plants.test.js
+++ __tests__/app_tests/app.plants.test.js
@@ -1,43 +1,25 @@
-import db from "../../db/connection";
-import data from "../../db/";
-import data from "../../db/data/test-data";
-import seed from "../../db/seeds/seed";
-import request from "supertest";
-import app from "../../app";
+const db = require("../../db/connection");
+const data = require("../../db/data/test_data/plants");
+const seed = require("../../db/seeds/seed");
+const request = require("supertest");
+const app = require("../../app");
 
 beforeEach(() => {
   return seed(data);
 });
 
 afterAll(() => {
-  return db.end;
+  return db.end();
 });
-
-/*
-/api/plants
-GET all species 
-returns an array of objects
-each object contains the keys: 
-/api/plants?species=none
-GET: all plants by species
-POST: new plant (with all above metrics)
-PATCH: individual plant (location?)
-DELETE: indivual plant
-
-api/plants/
-GET: all plants
-*/
 
 describe("/api/plants/", () => {
   describe("GET 200", () => {
-    test("responds with an object with a key of plants with a value of an array of objects", () => {
-      return request(app)
-        .get("api/plants/")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body).toBeObject();
-          expect(body.plants).toBeArray();
-        });
+    test("responds with an object with a key of plants with a value of an array of objects", async () => {
+      const { body } = await request(app).get("/api/plants/").expect(200);
+      console.log("body>>>", body);
+      expect(body).toBeObject();
+      expect(body.plants).toBeArray();
+      // .catch(err);
     });
   });
 });
