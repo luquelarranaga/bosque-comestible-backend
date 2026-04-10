caf97058c13080aa1bf13a4b26a680ed63fdc9f0
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-10 14:43:14 +0100

    created run-seed file

---
+++ db/seeds/run-seed.js
@@ -0,0 +1,11 @@
+const devData = require("../data/development-data/index.js");
+const seed = require("./seed.js");
+const db = require("../connection.js");
+
+const runSeed = async () => {
+  await seed(devData);
+  db.end();
+  return;
+};
+
+runSeed();
