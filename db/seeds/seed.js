f60192cc350624c6335fa664536d6eb1003e15a5
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-10 14:43:53 +0100

    amended seed.js file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,7 @@
-import db from "../connection";
-import format from "pg-format";
+const db = require("../connection");
+const format = require("pg-format");
 
-export const seed = async ({ plants }) => {
+const seed = async (plants) => {
   await db.query("DROP TABLE IF EXISTS plants");
 
   await db.query(`
@@ -14,17 +14,27 @@ export const seed = async ({ plants }) => {
     care VARCHAR,
     img_url VARCHAR
 )`);
+
+  const formattedPlants = plants.map((plant) => {
+    return [
+      plant.species,
+      plant.date_planted,
+      plant.coordinates,
+      plant.body,
+      plant.care,
+      plant.img_url,
+    ];
+  });
+
+  const plantsQueryStr = format(
+    `INSERT INTO plants
+    (species, date_planted, coordinates, body, care, img_url)
+    VALUES %L
+    RETURNING *;`,
+    formattedPlants,
+  );
+
+  await db.query(plantsQueryStr);
 };
 
-const insertPlantsQuery = format(
-  `INSERT INTO plants (species, date_planted, coordinates, body, care, img_url) VALUE %L RETURNING *;`,
-  plants.map(({ species, date_planted, coordinates, body, care, img_url }) => [
-    species,
-    date_planted,
-    coordinates,
-    body,
-    care,
-    img_url,
-  ]),
-);
-await db.query(insertPlantsQuery);
+module.exports = seed;
