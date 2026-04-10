2aaa945db2873049afc718d2d02648af5b115fd8
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-10 14:53:03 +0100

    created test to check correct shape of plant objects

--- __tests__/app_tests/app.plants.test.js
+++ __tests__/app_tests/app.plants.test.js
@@ -12,6 +12,14 @@ afterAll(() => {
   return db.end();
 });
 
+/*
+plant.species,
+      plant.date_planted,
+      plant.coordinates,
+      plant.body,
+      plant.care,
+      plant.img_url,
+*/
 describe("/api/plants/", () => {
   describe("GET 200", () => {
     test("responds with an object with a key of plants with a value of an array of objects", async () => {
@@ -19,7 +27,18 @@ describe("/api/plants/", () => {
       console.log("body>>>", body);
       expect(body).toBeObject();
       expect(body.plants).toBeArray();
-      // .catch(err);
+    });
+    test("each plant object contains correct keys", async () => {
+      const { body } = await request(app).get("/api/plants/").expect(200);
+      const { plants } = body;
+      plants.forEach((plant) => {
+        expect(typeof plant.species).toBe("string");
+        expect(typeof plant.date_planted).toBe("string");
+        expect(typeof plant.coordinates).toBe("string");
+        expect(typeof plant.body).toBe("string");
+        expect(typeof plant.care).toBe("string");
+        expect(typeof plant.img_url).toBe("string");
+      });
     });
   });
 });
