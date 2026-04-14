7f06f67135ea124cca567e61a15afe28de1c30f4
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-14 17:51:25 +0100

    updated model layer so that when species is queried, plants are returned ordered by date_planted

--- models/plants_models.js
+++ models/plants_models.js
@@ -2,9 +2,10 @@ const db = require("../db/connection");
 
 const fetchAllPlants = async (species) => {
   if (species) {
-    const result = await db.query(`SELECT * FROM plants WHERE species = $1`, [
-      species,
-    ]);
+    const result = await db.query(
+      `SELECT * FROM plants WHERE species = $1 ORDER BY date_planted DESC`,
+      [species],
+    );
     const { rows } = result;
     return rows;
   } else {
