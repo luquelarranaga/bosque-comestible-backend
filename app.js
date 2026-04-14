7063e2bbeafafb75dd89fb2a9d3e09d8187bb88f
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-14 14:11:19 +0100

    added error handling in app.js

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ app.use(express.json());
 app.use("/api/plants", plantsRouter);
 
 module.exports = app;
+
+app.use((err, req, res, next) => {
+  if (err instanceof NotFoundError) {
+    res.status(404).send({ msg: err.message });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof InvalidInputError) {
+    res.status(400).send({ msg: err.message });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal Server Error" });
+});
