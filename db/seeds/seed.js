f5308360ad3dbbe527229da3720219ef1a7211ef
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-24 10:15:29 +0100

    updated seed function to reflect changes in database schema

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,40 +1,76 @@
 const db = require("../connection");
 const format = require("pg-format");
 
-const seed = async (plants) => {
+const seed = async ({ plantsData, logsData, imagesData }) => {
+  await db.query("DROP TABLE IF EXISTS logs");
+  await db.query("DROP TABLE IF EXISTS images");
   await db.query("DROP TABLE IF EXISTS plants");
 
   await db.query(`
     CREATE TABLE plants (
     plant_id SERIAL PRIMARY KEY,
     species VARCHAR NOT NULL,
-    date_planted TIMESTAMP,
-    coordinates VARCHAR,
-    body VARCHAR,
-    care VARCHAR,
-    img_url VARCHAR
+    coordinates VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )`);
 
-  const formattedPlants = plants.map((plant) => {
-    return [
-      plant.species,
-      plant.date_planted,
-      plant.coordinates,
-      plant.body,
-      plant.care,
-      plant.img_url,
-    ];
+  await db.query(`
+    CREATE TABLE logs (
+    log_id SERIAL PRIMARY KEY,
+    plant_id INT REFERENCES plants(plant_id) ON DELETE CASCADE,
+    date_posted VARCHAR NOT NULL,
+    body VARCHAR
+)`);
+
+  await db.query(`
+    CREATE TABLE images (
+    image_id SERIAL PRIMARY KEY,
+    plant_id INT REFERENCES plants(plant_id) ON DELETE CASCADE,
+    date_taken VARCHAR NOT NULL,
+    img_url VARCHAR NOT NULL
+)`);
+
+  const formattedPlants = plantsData.map((plant) => {
+    return [plant.species, plant.coordinates, plant.created_at];
   });
 
   const plantsQueryStr = format(
     `INSERT INTO plants
-    (species, date_planted, coordinates, body, care, img_url)
+    (species, coordinates, created_at)
     VALUES %L
     RETURNING *;`,
     formattedPlants,
   );
 
   await db.query(plantsQueryStr);
+
+  const formattedLogs = logsData.map((log) => {
+    return [log.plant_id, log.date_posted, log.body];
+  });
+
+  const logsQueryStr = format(
+    `INSERT INTO logs
+    (plant_id, date_posted, body)
+    VALUES %L
+    RETURNING *;`,
+    formattedLogs,
+  );
+
+  await db.query(logsQueryStr);
+
+  const formattedImages = imagesData.map((image) => {
+    return [image.plant_id, image.date_taken, image.img_url];
+  });
+
+  const imagesQueryStr = format(
+    `INSERT INTO images
+    (plant_id, date_taken, img_url)
+    VALUES %L
+    RETURNING *;`,
+    formattedImages,
+  );
+
+  await db.query(imagesQueryStr);
 };
 
 module.exports = seed;
