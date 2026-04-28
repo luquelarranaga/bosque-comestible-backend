bac5c45e592fd0c29809a8479994b2362af14404
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-28 18:40:27 +0100

    set up hosting to supabase

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,20 @@
+const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("No PGDATABASE or DATABASE_URL configured");
+} else {
+  console.log(`Connected to database`);
+}
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  database:
-    process.env.NODE_ENV === "test"
-      ? "bosque_comestible_test"
-      : "bosque_comestible",
-});
-
-module.exports = pool;
+module.exports = new Pool(config);
